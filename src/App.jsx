6ed694cae90bba2ce4731eb954d89be198a6466f
J0hn316 <jcordell316@gmail.com>
2024-07-17 16:34:37 -0400

    set up style components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
+import Row from './ui/Row';
+
+const StyledApp = styled.div`
+  background-color: gray;
+  padding: 20px;
+`;
+
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Button>Click Me</Button>
+              <Input type="number" placeholder="Number of guests" />
+            </div>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
   );
 }
